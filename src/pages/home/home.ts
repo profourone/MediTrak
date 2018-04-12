bb062a522f7ab928996ac2b63c2d1a7b9f84652a
unknown <123cyyyyr@gmail.com>
2018-04-12 16:23:34 +0300

    Home screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+	//Default text
+	public mainMessage = '..\\..\\..\\assets\\imgs\\Congrats.png';	
+	public message = 'Don\'t lose\nYour fire\nKeep it up';
+	public messageNumber;
+	public streakNumber = 8;
+	
+  constructor(public navCtrl: NavController) {	
+	//TODO: update streak number here
+	this.streakNumber=2 ;
+	//Set the message for the streak
+	this.messageNumber = this.streakNumber > 1 ? 'Days Straight':'Day Straight';
+	//hide the picture + text
+	if (this.streakNumber <= 0){
+		this.mainMessage = "";
+		this.message = "";
+	}
   }
-
+  
 }
